5e332028f824869b86197e8b6f7e28d8eb34c6e6
zhangziraio8575 <polite.ridden2n@icloud.com>
2025-11-22 17:20:06 +0100

    Add files via upload

---
+++ start-mining-corecoin.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm randomy --pool corecoin.luckypool.io:3118 --wallet corecoin-wallet
+pause
