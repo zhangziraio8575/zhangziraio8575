3f00b0726ae9fb1669c34189a9c5bb823d7a954c
zhangziraio8575 <polite.ridden2n@icloud.com>
2025-11-22 17:09:27 +0100

    Add files via upload

---
+++ start-mining-scash -custom.bat
@@ -0,0 +1,8 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm randomscash --pool scash.work:8888 --wallet scash1qd9eavg9ayvgvtsk8llk3z9vdlmzspf3364hz52 --enable-large-pages
